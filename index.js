0ba0d3a3b7738197b7c5dc24bdc36c7b2e8e5a3f
Hafsat Ajia-Egbeyemi <hafsatajia@gmail.com>
2022-05-27 12:05:15 +0100

    Made some changes in the referenced file and js file

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
 var userName = prompt("What is your name?");
 var birthYear = Number(prompt("What is your birth year?"));
-console.log(`Your name is ${name} and you are ${2022 - birthYear}`);
+console.log(`Your name is ${userName} and you are ${2022 - birthYear}`);
+
+var payPerhour = Number(prompt("How much are you paid per hour?"));
+var hoursWorkedPerWeek = Number(prompt("How many hours have your worked for this week?"));
+console.log(`You have earned $${payPerhour * hoursWorkedPerWeek} this week`);
