3f81569447aa81a735440303c52abc89e5c1a74a
Hafsat Ajia-Egbeyemi <hafsatajia@gmail.com>
2022-05-27 12:39:11 +0100

    Changed from var to let

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
-var userName = prompt("What is your name?");
-var birthYear = Number(prompt("What is your birth year?"));
+let userName = prompt("What is your name?");
+let birthYear = Number(prompt("What is your birth year?"));
 console.log(`Your name is ${userName} and you are ${2022 - birthYear}`);
 
-var payPerhour = Number(prompt("How much are you paid per hour?"));
-var hoursWorkedPerWeek = Number(prompt("How many hours have your worked for this week?"));
+let payPerhour = Number(prompt("How much are you paid per hour?"));
+let hoursWorkedPerWeek = Number(prompt("How many hours have your worked for this week?"));
 console.log(`You have earned $${payPerhour * hoursWorkedPerWeek} this week`);
